3aa2da6bcdea1f6f1ab916355934fd8e338525cc
davi campos <dcr23@inf.ufpr.br>
2024-12-12 23:07:04 -0300

    added likwid support

--- ajustePol.c
+++ ajustePol.c
@@ -4,6 +4,7 @@
 #include <fenv.h>
 #include <math.h>
 #include <stdint.h>
+#include <likwid.h>
 
 #include "utils.h"
 
@@ -12,118 +13,126 @@
 /////////////////////////////////////////////////////////////////////////////////////
 
 void montaSL(double **A, double *b, int n, long long int p, double *x, double *y) {
-  for (int i = 0; i < n; ++i)
-    for (int j = 0; j < n; ++j) {
-      A[i][j] = 0.0;
-      for (long long int k = 0; k < p; ++k) {
-	A[i][j] += pow(x[k], i+j);
-      }
-    }
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j) {
+            A[i][j] = 0.0;
+            for (long long int k = 0; k < p; ++k) {
+                A[i][j] += pow(x[k], i+j);
+            }
+        }
 
-  for (int i = 0; i < n; ++i) {
-    b[i] = 0.0;
-    for (long long int k = 0; k < p; ++k)
-      b[i] += pow(x[k],i) * y[k];
-  }
+    for (int i = 0; i < n; ++i) {
+        b[i] = 0.0;
+        for (long long int k = 0; k < p; ++k)
+            b[i] += pow(x[k],i) * y[k];
+    }
 }
 
 void eliminacaoGauss(double **A, double *b, int n) {
-  for (int i = 0; i < n; ++i) {
-    int iMax = i;
-    for (int k = i+1; k < n; ++k)
-      if (A[k][i] > A[iMax][i])
-	iMax = k;
-    if (iMax != i) {
-      double *tmp, aux;
-      tmp = A[i];
-      A[i] = A[iMax];
-      A[iMax] = tmp;
+    for (int i = 0; i < n; ++i) {
+        int iMax = i;
+        for (int k = i+1; k < n; ++k)
+            if (A[k][i] > A[iMax][i])
+                iMax = k;
+        if (iMax != i) {
+            double *tmp, aux;
+            tmp = A[i];
+            A[i] = A[iMax];
+            A[iMax] = tmp;
 
-      aux = b[i];
-      b[i] = b[iMax];
-      b[iMax] = aux;
-    }
+            aux = b[i];
+            b[i] = b[iMax];
+            b[iMax] = aux;
+        }
 
-    for (int k = i+1; k < n; ++k) {
-      double m = A[k][i] / A[i][i];
-      A[k][i]  = 0.0;
-      for (int j = i+1; j < n; ++j)
-	A[k][j] -= A[i][j]*m;
-      b[k] -= b[i]*m;
+        for (int k = i+1; k < n; ++k) {
+            double m = A[k][i] / A[i][i];
+            A[k][i]  = 0.0;
+            for (int j = i+1; j < n; ++j)
+                A[k][j] -= A[i][j]*m;
+            b[k] -= b[i]*m;
+        }
     }
-  }
 }
 
 void retrossubs(double **A, double *b, double *x, int n) {
-  for (int i = n-1; i >= 0; --i) {
-    x[i] = b[i];
-    for (int j = i+1; j < n; ++j)
-      x[i] -= A[i][j]*x[j];
-    x[i] /= A[i][i];
-  }
+    for (int i = n-1; i >= 0; --i) {
+        x[i] = b[i];
+        for (int j = i+1; j < n; ++j)
+            x[i] -= A[i][j]*x[j];
+        x[i] /= A[i][i];
+    }
 }
 
 double P(double x, int N, double *alpha) {
-  double Px = alpha[0];
-  for (int i = 1; i <= N; ++i)
-    Px += alpha[i]*pow(x,i);
-  
-  return Px;
+    double Px = alpha[0];
+    for (int i = 1; i <= N; ++i)
+        Px += alpha[i]*pow(x,i);
+
+    return Px;
 }
 
 int main() {
 
-  int N, n;
-  long long int K, p;
+    int N, n;
+    long long int K, p;
 
-  scanf("%d %lld", &N, &K);
-  p = K;   // quantidade de pontos
-  n = N+1; // tamanho do SL (grau N + 1)
+    scanf("%d %lld", &N, &K);
+    p = K;   // quantidade de pontos
+    n = N+1; // tamanho do SL (grau N + 1)
 
-  double *x = (double *) malloc(sizeof(double)*p);
-  double *y = (double *) malloc(sizeof(double)*p);
+    double *x = (double *) malloc(sizeof(double)*p);
+    double *y = (double *) malloc(sizeof(double)*p);
 
-  // ler numeros
-  for (long long int i = 0; i < p; ++i)
-    scanf("%lf %lf", x+i, y+i);
+    // ler numeros
+    for (long long int i = 0; i < p; ++i)
+        scanf("%lf %lf", x+i, y+i);
 
-  double **A = (double **) malloc(sizeof(double *)*n);
-  for (int i = 0; i < n; ++i)
-    A[i] = (double *) malloc(sizeof(double)*n);
-  
-  double *b = (double *) malloc(sizeof(double)*n);
-  double *alpha = (double *) malloc(sizeof(double)*n); // coeficientes ajuste
+    double **A = (double **) malloc(sizeof(double *)*n);
+    for (int i = 0; i < n; ++i)
+        A[i] = (double *) malloc(sizeof(double)*n);
 
-  // (A) Gera SL
-  double tSL = timestamp();
-  montaSL(A, b, n, p, x, y);
-  tSL = timestamp() - tSL;
+    double *b = (double *) malloc(sizeof(double)*n);
+    double *alpha = (double *) malloc(sizeof(double)*n); // coeficientes ajuste
 
-  // (B) Resolve SL
-  double tEG = timestamp();
-  eliminacaoGauss(A, b, n); 
-  retrossubs(A, b, alpha, n); 
-  tEG = timestamp() - tEG;
+    LIKWID_MARKER_INIT;
 
-  for (int i = 0; i < n; i++){
-      for (int j = 0; j < n; j++)
-        printf("%1.15e\n ", A[i][j]);                                                                                                                                            
-  }
-  printf("matrix ^\n");
+    // (A) Gera SL
+    LIKWID_MAKER_START("montaSl");
+    double tSL = timestamp();
+    montaSL(A, b, n, p, x, y);
+    tSL = timestamp() - tSL;
+    LIKWID_MAKER_STOP("montaSl");
+
+    // (B) Resolve SL
+    LIKWID_MAKER_START("resolveSistema");
+    double tSL = timestamp();
+    double tEG = timestamp();
+    eliminacaoGauss(A, b, n); 
+    retrossubs(A, b, alpha, n); 
+    tEG = timestamp() - tEG;
+    LIKWID_MAKER_STOP("resolveSistema");
+
+    LIKWID_MARKER_CLOSE;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++)
+            printf("%1.15e\n ", A[i][j]);                                                                                                                                            
+    }
+    printf("matrix ^\n");
 
 
-  // Imprime coeficientes
-  for (int i = 0; i < n; ++i)
-    printf("%1.15e ", alpha[i]);
-  puts("");
+    // Imprime coeficientes
+    for (int i = 0; i < n; ++i)
+        printf("%1.15e ", alpha[i]);
+    puts("");
 
-  // Imprime resíduos
-  for (long long int i = 0; i < p; ++i)
-    printf("%1.15e ", fabs(y[i] - P(x[i],N,alpha)) );
-  puts("");
+    // Imprime resíduos
+    for (long long int i = 0; i < p; ++i)
+        printf("%1.15e ", fabs(y[i] - P(x[i],N,alpha)) );
+    puts("");
 
-  // Imprime os tempos
-  printf("%lld %1.10e %1.10e\n", K, tSL, tEG);
+    // Imprime os tempos
+    printf("%lld %1.10e %1.10e\n", K, tSL, tEG);
 
-  return 0;
+    return 0;
 }
